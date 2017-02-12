fb529f3b5653b0ea18c4a6f4c3e30dbbca089492
Uyen Lam <uyenlam17@u.northwestern.edu>
2017-02-12 12:48:23 -0600

    Added content to all handlebars

--- app/public/js/confirm.js
+++ app/public/js/confirm.js
@@ -1,35 +1,62 @@
-var dataOne  = {
-    "lat" : "",
-    "long" : ""
-};
-$("submit").click(function() {
+$(document).ready(function(){
+
+  $("#submit").click(function() {
+    //grab user's input
+    var userProfile = {
+      "name": $('#user-name').val().trim(),
+      "age": $('#user-age').val().trim(),
+      "occupation":$('#user-occu').val().trim(),
+      "photoLink": $('#user-image').val().trim(),
+      "vegetarian": $('#veg').val(),
+      "differentDiet":$('#diet').val(),
+      "favFood":$('#fav-food').val().trim(),
+      "leastFood":$('#least-food').val().trim(),
+      "favDrink":$('#fav-drink').val().trim(),
+      "leastDrink":$('#least-drink').val().trim(),
+      "introExtro": $('input:radio[name=in-ex]:checked').attr('id'),
+      "freeTime":$('#free-time').val().trim(),
+      "payView":$('input:radio[name=pay]:checked').attr('id'),
+      "cookView":$('input:radio[name=cook]:checked').attr('id'),
+      "minAvail":$('min-avail').val(),
+      "location":[
+        "lat":"",
+        "long":"",
+      ],
+      "locationName":"",
+    };
 
     function locate() {
         navigator.geolocation.getCurrentPosition(initialize, fail);
-    }
+    };
 
     function initialize(position) {
         var myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        //console.log(position.coords.latitude); 
+        //console.log(position.coords.latitude);
         var mapOptions = {
             zoom: 17,
             center: myLatLng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         console.log(myLatLng.lng(), myLatLng.lat());
-        dataOne.lat = myLatLng.lat();
-        dataOne.long = myLatLng.lng();
+        userProfile.location.lat = myLatLng.lat();
+        userProfile.location.long = myLatLng.lng();
     }
-locate(); 
+
+    locate().then(function(){
+      $.ajax( {
+          data: userProfile.location,
+          url: "/confirm",
+          method: "GET"
+        }).done(function(res){
+            userProfile.locationName = res,
+      //WHATEVER UYENS PROCESS IS FOR DISPLAYING THIS ON THE SCREEN;
+        });
+    }).then(function(){
+      $.post({
+        "/api/newuser",
+        ,
+        window.location.replace('/findpeople'),
+      });
+    };
+  });
 });
-
-
-
-$.ajax( { 
-    data: dataOne,  
-    url: "/confirm", 
-    method: "GET"}).done(function(res){ 
-
-//WHATEVER UYENS PROCESS IS FOR DISPLAYING THIS ON THE SCREEN; 
-
-    }); 
