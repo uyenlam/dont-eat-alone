e8a5a87f7040fd8f7c87f4ba6db7bc157a0a17b2
Chad Chaney <cchane4@Chads-MacBook-Air.local>
2017-02-12 06:23:51 -0600

    integration of files from testing

--- app/public/js/confirm.js
+++ app/public/js/confirm.js
@@ -1,4 +1,4 @@
-var dataOne  = {
+/*var dataOne  = {
     "lat" : "",
     "long" : ""
 };
@@ -33,3 +33,75 @@ $.ajax( {
 //WHATEVER UYENS PROCESS IS FOR DISPLAYING THIS ON THE SCREEN; 
 
     }); 
+
+*///commented out all the code above this to make way for a new confirm.js file (chad)  
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Google Maps User Location</title>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-495IBdppX1la6lBN5KTa8-bQG8_6UCQsensor=false"></script>
+    <style>
+    html, body, #map_canvas{width:100%;height:100%;}
+    </style>
+  </head>
+  <body>
+    <ul id="restaurantList">
+    </ul>
+
+
+
+
+</body>
+<script>
+function xhrGet(route, cb) {
+   var request = new XMLHttpRequest();
+   request.open('GET', route, true);
+   request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+         var resp = request.responseText;
+
+         cb(JSON.parse(resp));
+      } else {
+
+      }
+   };
+   request.onerror = function() {
+      console.log('things happened');
+   };
+   request.send();
+
+};
+
+</script>
+<script>
+    var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
+    var restaurantList = document.getElementById('restaurantList');
+    function locate(){
+        navigator.geolocation.getCurrentPosition(initialize,fail);
+    }
+
+    function initialize(position) {
+        console.log('intialize func position',position);
+        var myLatLng = {lat: position.coords.latitude, lng:position.coords.longitude };
+        console.log('myLatLng', myLatLng);
+        var nearbyRoute = '/nearby/'+ position.coords.latitude +'/' + position.coords.longitude;
+        xhrGet(nearbyRoute, function(result) {
+          console.log('result', result);
+          if(result === 'error') return;
+        console.log(result.nearby_restaurants[0])
+        restaurantList.innerHTML =  result.nearby_restaurants.map(function(item, index){
+              return '<li><strong>' + item.restaurant.name +'</strong></li>';
+         }).join('')
+        })
+      }
+
+
+     function fail(){
+         alert('navigator.geolocation failed, may not be supported');
+     }
+     locate();
+  </script>
+</html>
--- server.js
+++ server.js
@@ -8,6 +8,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
+var express      = require('express');
+var fs           = require('fs');
+var ejs          = require('ejs');
+var app          = new express();
+var bodyParser   = require('body-parser');
+var http         = require('http');
+var path         = require('path');
+var zomato       = require('zomato');
+var env          = process.env;
+
 //
 // var passport = require('passport');
 // var session = require('express-session');
@@ -28,12 +38,25 @@ var exphbs = require("express-handlebars");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+// sets up zomato api integrated this code from index.ejs (chad)
+var client = zomato.createClient({
+  userKey: '67439a2a6001f3cc23b26aba575a54ff', //as obtained from [Zomato API](https://developers.zomato.com/apis)
+});
+// integrated this code from index.ejs (chad)
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// the following 4 lines of code were integrated (chad)
+app.enable('trust proxy');
+//app.use(express.static(__dirname + '/public')); commented out (chad)
+//app.set('view engine', 'ejs'); commented out because this integration is not needed bc we are using handlebars (chad)
+//app.set('views', __dirname + '/public'); commented out (chad)
 
 // Static directory
 app.use(express.static("app/public"));
@@ -41,6 +64,7 @@ app.use(express.static("app/public"));
 //
 // // Requiring our models for syncing
 var db = require("./app/models"); //requiring the whole model.
+var request = require('request');
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -48,10 +72,52 @@ app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
+/*get route for confirm
+app.get("/confirm", function(req, res){ 
+
+	request('http://www.google.com', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    // console.log(body) // Show the HTML for the Google homepage. 
+	    res.end(body);
+	  }
+	})
+
+}); 
+*/ // commented out this part this is old code from sean (chad)
+
+//INTEGRATE THIS INTO FILES (chad)
+app.get('/', function (req,res) {
+	res.render('confirm');
+}); // changed res.render('index') which references index.ejs to confirm which references confirm.js (chad)
+// also changed es6 syntax to vanilla javascript
+app.get('/nearby/:lat/:lon', function(req, res){
+
+    console.log('req.params', req.params); //added semicolon here (chad)
+  client.getGeocode({
+    lat:req.params.lat,
+    lon:req.params.lon,
+    }, function(err, result){
+        if(!err){
+          res.send(result);
+        }else {
+            res.send('error');
+          console.log(err);
+        }
+  });
+
+}); ///added semicolon here (chad)
+
+
+/*var server = app.listen(env.NODE_PORT || 8000, env.NODE_IP || 'localhost', () => {
+	console.log('At the URL: http://localhost:8000');
+}); */  
+
+
+/*Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+//require("./app/routes/api-routes.js")(app);
+//require("./app/routes/html-routes.js")(app);*/ 
+//commented out this part bc we're not using a routes page (chad)
 
 
 // Starts the server to begin listening
